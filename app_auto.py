e4d80652c2e80c267c814b79e271a834716f342c
AndreAguiar <andreaguiar90@gmail.com>
2024-04-02 20:59:28 -0300

    Atualização
    Imagens adicionadas substituindo os cliques por coordenadas.

--- app_auto.py
+++ app_auto.py
@@ -44,10 +44,10 @@ def iniciar():
     try:
         sleep(1)
         # Mostra os itens do pedido na tela de preparação
-        #pyautogui.click(1200,317,duration=0.5)
+        #pyautogui.click(pyautogui.locateCenterOnScreen('img/mostrar.png', minSearchTime=2),duration=0.2)
         
         # Processa os itens conferindo se tem os lotes informados
-        pyautogui.click(1197,342,duration=0.5) 
+        pyautogui.click(pyautogui.locateCenterOnScreen('img/processar.png', minSearchTime=2),duration=0.2)
         sleep(0.5)
 
         # Transfere os itens para a nota
@@ -115,8 +115,7 @@ def iniciar():
                 # Preenche cada linha com os volumes informados 
                 for item in item_sep:
                     pyautogui.write(str(item))
-                    pyautogui.press('down')
-                    sleep(0.7)
+                    pyautogui.press('down', interval=0.5)
                 
                 # Pressiona Esc para sair da última linha criada em branco
                 pyautogui.press('esc')
@@ -130,56 +129,55 @@ def iniciar():
         
         # Tela da NF
         # Clica em diversos para preencher transportadora, redespacho, volumes, e frases
-        pyautogui.click(64,144,duration=0.2) 
+        pyautogui.click(pyautogui.locateCenterOnScreen('img/diversos.png', minSearchTime=2),duration=0.2) 
         sleep(1.3)
         
         # Transportadora
-        pyautogui.doubleClick(225,87,duration=0.2) 
+        camp_transp = pyautogui.locateCenterOnScreen('img/campo_transportadora.png', minSearchTime=1)
+        pyautogui.doubleClick(camp_transp[0]+120,camp_transp[1],duration=0.2)
         pyautogui.write(transportadora)
-
+        pyautogui.press('enter', presses=4, interval=0.1)
+        
         # Verifica se tem transportadora informada como redespacho e preenche no campo
         if redespacho == '0' or redespacho == '':
             tem_redespacho = False
         else:
             tem_redespacho = True
-            pyautogui.click(220,163,duration=0.2)
             pyautogui.write(redespacho)
 
+        pyautogui.press('enter', presses=4, interval=0.1)
+        
         # Quantidade de volumes
-        pyautogui.click(190,211,duration=0.2) 
         pyautogui.write(volume)
+        pyautogui.press('enter')
 
         # Tipo de volume
-        pyautogui.click(229,211,duration=0.2) 
         pyautogui.write(especie)
-
-        # Preenche as frases
-        pyautogui.doubleClick(173,266,duration=0.2) 
+        pyautogui.press('enter', presses=3, interval=0.1)
+        
+        # Preenche as frases 
         for frase in frases:        
             pyautogui.write(str(frase))
-            pyautogui.keyDown('enter')
-            pyautogui.keyDown('enter')
-            sleep(0.3)
+            pyautogui.press('enter', presses=2, interval=0.1)
         
         # Confirmação de preenchimento
         messagebox.showinfo('Atenção', 'Confira os dados!')
 
         # Processar
-        pyautogui.click(1324,38,duration=0.2) 
+        pyautogui.click(pyautogui.locateCenterOnScreen('img/processar.png', minSearchTime=2),duration=0.2)
 
         # Adiciona o tipo de frete do redespacho caso tenha transportadora informada
         if tem_redespacho: 
-            pyautogui.click(1244,109,duration=0.2) 
-            pyautogui.doubleClick(519,340,duration=0.5)
+            pyautogui.click(pyautogui.locateCenterOnScreen('img/person.png', minSearchTime=2),duration=0.2) 
             pyautogui.write('F')
-            pyautogui.keyDown('enter')
-            pyautogui.click(998,234,duration=0.5)
+            pyautogui.press('enter', interval=0.3)
+            pyautogui.hotkey('alt','s')
 
         # Fechar NF
-        pyautogui.click(1246,61,duration=0.3)
+        pyautogui.click(pyautogui.locateCenterOnScreen('img/fechar.png', minSearchTime=2),duration=0.2) 
         sleep(0.5)
         
-        # Variáveis usadas no looping
+        # Variáveis condicionais do looping
         fechar = True
         sim = True
         continuar = True
@@ -210,23 +208,23 @@ def iniciar():
 
             # Imprimir certificado caso tenha         
             try:
-                clica_imprimir = pyautogui.locateOnScreen('img/imprimir.png', minSearchTime=1, region=(418,0,656,63))
+                clica_imprimir = pyautogui.locateOnScreen('img/imprimir.png', minSearchTime=2, region=(418,0,656,63))
                 pyautogui.click(clica_imprimir, duration=0.2)
                 certificado=True
             except:
                 certificado = False
             if certificado==True:
-                clica_ok = pyautogui.locateOnScreen('img/ok.png', minSearchTime=1, region=(807,188,1037,352))
+                clica_ok = pyautogui.locateOnScreen('img/ok-cert.png', minSearchTime=1, region=(807,188,1037,352))
                 pyautogui.click(clica_ok, duration=0.3) # posição do ok 935,252
                 clica_sair = pyautogui.locateOnScreen('img/sair.png', minSearchTime=1.5, region=(418,0,656,63))
                 pyautogui.click(clica_sair, duration=0.2)
             
-            # Verifica se pode sair do looping
+            # Verifica se continua ou sai do looping
             if sim == False and continuar == False and certificado == False:
                 fechar = False
 
         # clica no Ok para continuar
-        clica_ok = pyautogui.locateOnScreen('img/ok-1.png', minSearchTime=0.3, region=(1193, 0, 1361, 271))
+        clica_ok = pyautogui.locateOnScreen('img/ok-esp.png', minSearchTime=0.3, region=(1193, 0, 1361, 271))
         pyautogui.click(clica_ok, duration=0.2)
         sleep(2)
 
@@ -259,7 +257,6 @@ def iniciar():
             abrirJanela()
         else:
             messagebox.showinfo('Aviso', 'Finalizado com sucesso!')
-            quit()
 
     except:
         messagebox.showwarning('Aviso', 'Execução encerrada!')
@@ -293,8 +290,8 @@ def abrirJanela():
     altura = 180
     largura_tela = janela.winfo_screenwidth()
     altura_tela =  janela.winfo_screenheight()
-    posix = largura_tela/2 - largura/2
-    posiy = altura_tela/2 - altura/2
+    posix = largura_tela/2
+    posiy = altura_tela/2 - altura
     janela.geometry("%dx%d+%d+%d" % (largura,altura,posix,posiy))
 
     # Label e campo de preenchimento da transportadora
